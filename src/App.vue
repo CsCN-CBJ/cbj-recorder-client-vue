e2cbe7a27c1d0d7fffa01d6c0f07474f6c6db49d
陈柏居 <200110705@stu.hit.edu.cn>
2024-04-21 15:35:54 +0800

    delete helloworld, add vant, add router, add light/dark switch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 深色模式 https://vant-contrib.gitee.io/vant/#/zh-CN/config-provider#shen-se-mo-shi -->
+  <van-config-provider :theme="theme" ></van-config-provider>
+  <VanButton type="primary" color="linear-gradient(to right, #ffffff, #000000)" block size="large" @click=onSwapTheme>深/浅</VanButton>
+  <br><br>
+  <VanButton type="primary" block size="large" @click=handleClick2>Home</VanButton>
+  <br><br>
+  <VanButton type="primary" block size="large" @click=handleClick1>Swap</VanButton>
+
+  <!-- 放具体路由页面的内容 -->
+  <RouterView></RouterView>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import router from './router';
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      theme: 'dark'
+    }
+  },
+  methods: {
+    onSwapTheme() {
+      this.theme = this.theme === 'dark' ? 'light' : 'dark';
+      console.log(process.env.VUE_APP_LEDGER_TOP_COMMON_MEALS)
+      console.log(process.env.VUE_APP_LEDGER_TOP_SNACKS)
+    },
+    handleClick1() {
+      router.replace('/about');
+    },
+    handleClick2() {
+      router.replace('/home');
+    },
   }
 }
+
 </script>
 
 <style>
@@ -22,5 +46,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.van-theme-dark body {
+  color: #f5f5f5;
+  background-color: black;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import home from '@/views/home.vue'
+import about from '@/views/about.vue'
+
+const router = createRouter(
+    {
+        history: createWebHistory(),
+        routes: [
+            { path: '/home', component: home },
+            { path: '/about', component: about }
+        ]
+    }
+)
+export default router;
