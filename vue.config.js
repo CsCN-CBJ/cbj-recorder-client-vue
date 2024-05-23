d70b18630886b1f2f0da935910f1988ecb33fa53
陈柏居 <200110705@stu.hit.edu.cn>
2024-05-23 22:33:58 +0800

    feat: add https

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const fs = require('fs')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
+    transpileDependencies: true,
+    lintOnSave: false,
+    devServer: {
+        allowedHosts: process.env.VUE_APP_ALLOWED_HOSTS.split(','),
+        port: process.env.VUE_APP_PORT,
+        https: {
+            key: fs.readFileSync(process.env.VUE_APP_HTTPS_KEY),
+            cert: fs.readFileSync(process.env.VUE_APP_HTTPS_CERT),
+        }
+    }
 })
