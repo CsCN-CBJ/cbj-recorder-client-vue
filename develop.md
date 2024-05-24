b74fba79775d3aeb9fbf837482f33456208bfe26
陈柏居 <200110705@stu.hit.edu.cn>
2024-05-24 20:51:23 +0800

    fix: 修复了控制台报WebSocket connection failed的问题

--- develop.md
+++ develop.md
@@ -7,3 +7,5 @@
 2024-5-19 13:01:07 基本完成了记账功能
 
 2024-5-22 20:02:42 实现了按钮选中后改变样式的功能, 判断当前选中的值, 并设置对应的`v-bind:class`
+
+2024-5-24 20:41:14 尝试解决控制台报`WebSocket connection to 'ws://xxx.xxx.x.xxx:8080/ws' failed:`的问题, 参考[WebSocket connection to ‘ws://x.x.x.x:8080/ws‘ failed:报错_websocket connection to ws failed-CSDN博客](https://blog.csdn.net/weixin_62328829/article/details/126995531), 原本采用的是设置`webSocketURL`的方法, 但是感觉不太好, 而且会有ws和wss的问题, 见[http/https与websocket的ws/wss的关系](https://blog.csdn.net/Garrettzxd/article/details/81674251), 想了一下感觉用不到websocket, 所以最后采用`ws: false`的方法, 全面禁止websocket.
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,16 @@ const {defineConfig} = require('@vue/cli-service')
 const fs = require('fs')
 module.exports = defineConfig({
     transpileDependencies: true,
-    lintOnSave: false,
+    lintOnSave: false, // 关闭语法检查
     devServer: {
         allowedHosts: process.env.VUE_APP_ALLOWED_HOSTS.split(','),
         port: process.env.VUE_APP_PORT,
         https: {
             key: fs.readFileSync(process.env.VUE_APP_HTTPS_KEY),
             cert: fs.readFileSync(process.env.VUE_APP_HTTPS_CERT),
-        }
+        },
+        proxy: {
+            ws: false,
+        },
     }
 })
